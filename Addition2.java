dd171dbb2a74b3ec0a6aa0ccd6474f6d266253c7
Parshuram <admin@Parshuram.Parshuram>
2024-07-29 19:42:30 +0530

    Successfully Added

---
+++ Addition2.java
@@ -0,0 +1,40 @@
+class AdditionA{
+  public static void main(String[] args){
+   int d1=20;
+   double i1=30.5;
+   double i2=d1+i1;
+  System.out.println(d1);
+  System.out.println(i1);
+  System.out.println(i2);
+ }
+}
+class AdditionB{
+  public static void main(String[] args){
+   int d1=20;
+   double i1=30.5;
+   double i2=d1+i1;
+  System.out.println(d1);
+  System.out.println(i1);
+  System.out.println(i2);
+ }
+}
+class AdditionC{
+  public static void main(String[] args){
+   int d1=20;
+   double i1=30.5;
+   double i2=d1+i1;
+  System.out.println(d1);
+  System.out.println(i1);
+  System.out.println(i2);
+ }
+}
+class AdditionD{
+  public static void main(String[] args){
+   int d1=20;
+   double i1=30.5;
+   double i2=d1+i1;
+  System.out.println(d1);
+  System.out.println(i1);
+  System.out.println(i2);
+ }
+}
