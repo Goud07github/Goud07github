9e337bdd294b9503fda17a2759dd9e6fbc61e89f
Parshuram <admin@Parshuram.Parshuram>
2024-07-29 18:26:27 +0530

    added

---
+++ Example1.java
@@ -0,0 +1,8 @@
+class Example1{
+  public static void main(String[] args){
+    System.out.println("start main");
+    System.out.println(100/20);
+    System.out.println(100/5);
+    System.out.println("start end");
+  }
+}
