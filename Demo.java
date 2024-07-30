5dd4eb08856175eaa83e0cb41b1a450b1a352eae
Parshuram <admin@Parshuram.Parshuram>
2024-07-30 09:13:52 +0530

    added modify project

--- Demo.java
+++ Demo.java
@@ -1,8 +1,12 @@
 public class Demo{
-
+   int a=100;
+  int b= 10;
 public void m1(){
-
-
+   int c=a/b;
+  system.out.println(c);
 }
+public Static void main(String [] args){
+ system.out println("print the c value");
 
+  }
 }
